32c9d67077879ecf607b27642e146c20c1bb99ec
NikitaPalvankar <87067356+NikitaPalvankar@users.noreply.github.com>
2025-10-22 15:11:16 +0530

    Initial commit

--- day2/Palindrome.java
+++ day2/Palindrome.java
@@ -6,7 +6,6 @@ public class Palindrome {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a number: ");
 		int num = sc.nextInt();
