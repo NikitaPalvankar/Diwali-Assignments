bf97f1c75a4362578fa50e7ce1cf8e525512e1c6
Nikita <nikita2001palvankar@gmail.com>
2025-10-24 17:31:51 +0530

    Initial Commit

---
+++ study/Calculator.java
@@ -0,0 +1,29 @@
+package study;
+
+
+public class Calculator {
+
+	  public static int calculate(int number1, int number2, String operator) throws WrongOperatorException {
+	        switch (operator) {
+	            case "+":
+	                return number1 + number2;
+	            case "-":
+	                return number1 - number2;
+	            case "*":
+	                return number1 * number2;
+	            case "/":
+	                if (number2 == 0) {
+	                    throw new ArithmeticException("Division by zero is not allowed");
+	                }
+	                return number1 / number2;
+	            case "%":
+	                if (number2 == 0) {
+	                    throw new ArithmeticException("Modulo by zero is not allowed");
+	                }
+	                return number1 % number2;
+	            default:
+	                // If operator is invalid
+	                throw new WrongOperatorException("Invalid operator: " + operator);
+	        }
+	    }
+}
