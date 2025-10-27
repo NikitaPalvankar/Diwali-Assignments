baf27cf88d95fc3f0776962c5fa3184009858b27
NikitaPalvankar <87067356+NikitaPalvankar@users.noreply.github.com>
2025-10-27 08:49:23 +0530

    Initial commit

--- day2/Palindrome.java
+++ day2/Palindrome.java
@@ -15,7 +15,7 @@ public class Palindrome {
 
 	private static boolean reverseNum(int num) {
 		// TODO Auto-generated method stub
-		int rem=0, rev = 0;//4872
+		int rem=0, rev = 1;//4872
 		for (int i = num; i > 0; i=i /10) {
 			rem = i % 10;
 			rev= rev*10+rem;
