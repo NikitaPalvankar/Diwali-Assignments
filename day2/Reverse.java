21771dd88c5fb4c572759d47c5f4172d18f66628
NikitaPalvankar <87067356+NikitaPalvankar@users.noreply.github.com>
2025-10-27 08:59:17 +0530

    Initial commit

--- day2/Reverse.java
+++ day2/Reverse.java
@@ -14,7 +14,7 @@ public class Reverse {
 
 	private static void reverseNum(int num) {
 		// TODO Auto-generated method stub
-		int rem=0, rev = 0;//4872
+		int rem=0, rev = 1;//4872
 		for (int i = num; i > 0; i=i /10) {
 			rem = i % 10;
 			rev= rev*10+rem;
